970a0346b2b97fb9495ac08ac3c74f17950e2bd1
MahadiAnkon <ankon1807075@stud.kuet.ac.bd>
2022-06-24 21:20:04 +0600

    Detailed Page

--- app/src/main/java/com/example/cardiac_recorder/MainActivity.java
+++ app/src/main/java/com/example/cardiac_recorder/MainActivity.java
@@ -4,8 +4,11 @@ import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
