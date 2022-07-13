467a3b2fe44a1e460891cc97431b5431549641e5
MahadiAnkon <ankon1807075@stud.kuet.ac.bd>
2022-07-13 17:34:16 +0600

    add to and show content from database completed

--- app/src/main/java/com/example/cardiac_recorder/MainActivity.java
+++ app/src/main/java/com/example/cardiac_recorder/MainActivity.java
@@ -78,16 +78,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         contactsrecview = findViewById(R.id.contactsrecview);
-//        measurement.add(new Measurement("1st July","9.56pm",80,137,92,"Healthy"));
-//        measurement.add(new Measurement("2nd July","9.57pm",81,135,96,"Healthy"));
-//        measurement.add(new Measurement("3rd July","9.58pm",82,134,93,"Healthy"));
-//        measurement.add(new Measurement("4th July","9.59pm",83,134,99,"Needs Observation"));
-//        measurement.add(new Measurement("1st July","9.56pm",80,137,92,"Healthy"));
-//        measurement.add(new Measurement("2nd July","9.57pm",81,135,96,"Healthy"));
-//        measurement.add(new Measurement("3rd July","9.58pm",82,134,93,"Healthy"));
-//        measurement.add(new Measurement("4th July","9.59pm",83,134,99,"Needs Observation"));
-
-     //   FirebaseApp.initializeApp(this);
         databaseReference= FirebaseDatabase.getInstance().getReference();
         databaseReference.child("measurement").addValueEventListener(new ValueEventListener()
         {
