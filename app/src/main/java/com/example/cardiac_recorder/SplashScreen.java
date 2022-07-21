6ef3fbe98bf110a45cb6be811d82860e913bfa1a
Shafin <mahmood1807074@stud.kuet.ac.bd>
2022-07-21 22:52:10 +0600

    Splash Screen added.

---
+++ app/src/main/java/com/example/cardiac_recorder/SplashScreen.java
@@ -0,0 +1,54 @@
+package com.example.cardiac_recorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity
+{
+    ProgressBar progressBar;
+    Timer timer;
+    int i=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        progressBar=findViewById(R.id.splash_progress);
+        timer=new Timer();
+        timer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                if(i<100)
+                {
+                    runOnUiThread(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+
+                        }
+                    });
+                    progressBar.setProgress(i);
+                    i++;
+                }
+                else
+                {
+                    timer.cancel();
+                    Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        },0, 50);
+
+    }
+}
