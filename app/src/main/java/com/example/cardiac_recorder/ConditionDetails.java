e4ad0b6267a0de1e615bce40210f334422cb67c5
Shafin <mahmood1807074@stud.kuet.ac.bd>
2022-07-28 22:51:54 +0600

    Javadoc updated

--- app/src/main/java/com/example/cardiac_recorder/ConditionDetails.java
+++ app/src/main/java/com/example/cardiac_recorder/ConditionDetails.java
@@ -10,8 +10,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
